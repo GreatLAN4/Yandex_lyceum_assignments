c3858c8b3e19d0bb922f15607eafb2d447fb952d
GreatLAN <vildan987654321@gmIL.com>
2022-12-22 17:33:55 +0500

    PyQt lesson 6 new tasks 22.12 and some tasks

--- Основы промышленного программирования/QT 9. Обработка клавиатуры и курсора. Сборка независимого приложения/Супрематизм/Супрематизм.py
+++ Основы промышленного программирования/QT 9. Обработка клавиатуры и курсора. Сборка независимого приложения/Супрематизм/Супрематизм.py
@@ -1,45 +1,60 @@
+import math
 import sys
-
+from random import sample, randint
+from PyQt5.QtGui import QPainter, QColor, QPainterPath
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel
-from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.Paint = False
+        self.left, self.space, self.right = False, False, False
         self.initUI()
-
 
     def initUI(self):
         self.setGeometry(300, 300, 300, 300)
         self.setWindowTitle('Координаты')
 
-
     def mousePressEvent(self, event):
-        self.x, self.y = event.x(), event.x()
+        self.x, self.y = event.x(), event.y()
+
         if event.button() == Qt.LeftButton:
-            self.Paint, self.mode = True, 0
+            self.left = True
 
         elif event.button() == Qt.RightButton:
-            self.Paint, self.mode = True, 1
+            self.right = True
 
-    def Draw(self, qp):
-        if self.mode == 0:
-            qp.drawEllipse(30, 30, 200, 200)
-        elif self.mode == 1:
-            qp.drawRect(30, 30, 60, 60)
-        elif self.mode == 2:
-            qp.drawRect(30, 30, 60, 60)
+        self.repaint()
 
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Space:
+            self.space = True
 
     def paintEvent(self, event):
-        if self.Paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.Draw(qp)
-            qp.end()
+        qp = QPainter()
+        color = (sample(range(0, 256), 3))
+        color = QColor(color[0], color[1], color[2])
+        qp.begin(self)
+        qp.setBrush(color)
+        d = randint(10, 75)
+        r = d // 2
+        if self.left:
+            qp.drawEllipse(self.x - r, self.y - r, d, d)
+            self.left = False
+
+        elif self.right:
+            qp.drawRect(self.x - r, self.y - r, d, d)
+            self.right = False
+
+        elif self.space:
+            path = QPainterPath()
+            path.lineTo(self.x - r, self.y - r)
+            path.lineTo(self.x + r, self.y - r)
+            path.lineTo(self.x, self.y + r)
+            self.space = False
+        qp.end()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
