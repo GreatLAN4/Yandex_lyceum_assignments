d54853c7d96d7001aef20de06059fa106a641998
GreatLAN <vildan987654321@gmIL.com>
2023-01-11 22:23:44 +0500

    11.01.23 finished one program from PyQt 6

--- Основы промышленного программирования/QT 9. Обработка клавиатуры и курсора. Сборка независимого приложения/Супрематизм/Супрематизм.py
+++ Основы промышленного программирования/QT 9. Обработка клавиатуры и курсора. Сборка независимого приложения/Супрематизм/Супрематизм.py
@@ -1,24 +1,27 @@
 import math
 import sys
 from random import sample, randint
-from PyQt5.QtGui import QPainter, QColor, QPainterPath
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QColor, QPainterPath, QPolygon
+from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtWidgets import QWidget, QApplication
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.left, self.space, self.right = False, False, False
+        self.left, self.space, self.right = "", "", ""
         self.initUI()
 
     def initUI(self):
         self.setGeometry(300, 300, 300, 300)
         self.setWindowTitle('Координаты')
 
-    def mousePressEvent(self, event):
+    def mouseMoveEvent(self, event):
+        self.setMouseTracking(True)
         self.x, self.y = event.x(), event.y()
 
+    def mousePressEvent(self, event):
+        self.x, self.y = event.x(), event.y()
         if event.button() == Qt.LeftButton:
             self.left = True
 
@@ -30,12 +33,13 @@ class Example(QWidget):
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Space:
             self.space = True
+        self.repaint()
 
     def paintEvent(self, event):
         qp = QPainter()
+        qp.begin(self)
         color = (sample(range(0, 256), 3))
         color = QColor(color[0], color[1], color[2])
-        qp.begin(self)
         qp.setBrush(color)
         d = randint(10, 75)
         r = d // 2
@@ -48,10 +52,10 @@ class Example(QWidget):
             self.right = False
 
         elif self.space:
-            path = QPainterPath()
-            path.lineTo(self.x - r, self.y - r)
-            path.lineTo(self.x + r, self.y - r)
-            path.lineTo(self.x, self.y + r)
+            points = QPolygon([QPoint(self.x - r, self.y - r),
+                               QPoint(self.x, self.y + r),
+                               QPoint(self.x + r, self.y - r), ])
+            qp.drawPolygon(points)
             self.space = False
         qp.end()
 
