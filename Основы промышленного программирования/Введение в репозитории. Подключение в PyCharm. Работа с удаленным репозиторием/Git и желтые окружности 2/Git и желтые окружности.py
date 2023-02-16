9775ad3ac8074ea4e857bffc283142f046817dd5
GreatLAN <vildan987654321@gmIL.com>
2023-02-16 12:16:29 +0500

    fixed problem with folder and done one lesson next I'll dane game the game by 2 weeks

--- Основы промышленного программирования/Введение в репозитории. Подключение в PyCharm. Работа с удаленным репозиторием/Git и желтые окружности 2/Git и желтые окружности.py
+++ Основы промышленного программирования/Введение в репозитории. Подключение в PyCharm. Работа с удаленным репозиторием/Git и желтые окружности 2/Git и желтые окружности.py
@@ -1,5 +1,9 @@
 import sys
+from random import randint
+
 from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
@@ -8,18 +12,31 @@ class Circle(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("UI.ui", self)
-        self.prise_list = {}
+        self.amount, self.radius = 0, 0
 
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle('Интерактивный чек')
-        self.Button.clicked.connect(self.Run)
+        self.Button.clicked.connect(self.CalcCircle)
 
-    def Run(self):
-        print("clicked")
+
+    def CalcCircle(self):
+        amount = randint(1, 5)
+        radius = randint(10, 150)
+        self.amount, self.radius = amount, radius
+        self.repaint()
+
+
     def paintEvent(self, event):
-        pass
+        qp = QPainter()
+        qp.begin(self)
+        color = QColor(255, 255, 0)
+        qp.setBrush(color)
+        for i in range(self.amount):
+            qp.drawEllipse(randint(50, 400), randint(50, 400), self.radius, self.radius)
+
+        qp.end()
 
 
 
