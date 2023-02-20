3fbeb4de95ae31906080f1a38263b0a41221de6a
GreatLAN <vildan987654321@gmIL.com>
2023-02-20 22:30:42 +0500

    PyGame It took some time to solve 1st task from 2nd lesson 2 forgot to paste changing from draft file

--- Основы промышленного программирования/PyGame 2. Игровой цикл. События/Жёлтый круг.py
+++ Основы промышленного программирования/PyGame 2. Игровой цикл. События/Жёлтый круг.py
@@ -1,25 +1,43 @@
 import pygame
 
+
+def draw(screen, flag, x=0, y=0, ):
+    if flag:
+        pygame.draw.circle(screen, "yellow", (x, y), radius)
+    else:
+        pass
+
+
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Движущийся круг 2')
-    size = width, height = 800, 400
+    size = width, height = 500, 500
     screen = pygame.display.set_mode(size)
 
-    running = True
-    x_pos = 0
-    v = 20  # пикселей в секунду
-    fps = 60
+    running, expand = True, False
+    radius, x, y = 0, 0, 0
+    v = 20
+    fps = 120
+    screen.fill("blue")
+
     clock = pygame.time.Clock()
     while running:
-        screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        pygame.draw.circle(screen, (255, 0, 0), (int(x_pos), 200), 20)
+            if event.type == pygame.MOUSEMOTION:
+                mouse_x, mouse_y = event.pos
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                screen.fill("blue")
+                x, y = mouse_x, mouse_y
+                radius = 0
+                expand = True
+
+        draw(screen, expand, x, y)
 
         pygame.display.flip()
-        x_pos += v / fps
+        radius += v / fps
         clock.tick(fps)
 
     pygame.quit()
